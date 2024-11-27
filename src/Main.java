2762e738db04f9ac2ba8370f876a69f7714b6780
me-coderaider <kashyappankajnsp@gmail.com>
2024-11-27 16:20:21 +0530

    Singly LinkedList creation, inserted, delete, traverse & searching

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import singlylinkedlist.SinglyLinkedList;
+
 class Main {
 
     public static void main(String[] args) {
-        
+        // Singly LL
+        SinglyLinkedList sLL = new SinglyLinkedList();
+        sLL.createSinglyLinkedList(11);
+
+        // System.out.println(sLL.head.value);
+        sLL.insertInLinkedList(6, 1);
+        sLL.insertInLinkedList(7, 3);
+        sLL.insertInLinkedList(8, 4);
+        sLL.insertInLinkedList(9, 5);
+        sLL.insertInLinkedList(10, 2);
+        sLL.traverseSinglyLinkedList();
     }
 }
---
+++ src/singlylinkedlist/Node.java
@@ -0,0 +1,6 @@
+package singlylinkedlist;
+
+public class Node {
+	public int value;
+	public Node next;
+}
