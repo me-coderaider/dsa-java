60fda533888e23004c147766036b4e64a4d6dd38
me-coderaider <kashyappankajnsp@gmail.com>
2024-12-06 14:57:03 +0530

    Sorting Algorithm -- Bubble Sort

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,18 @@
-import hashing.direct_chaining.DirectChaining;
-import hashing.open_addressing.DoubleHashing;
-import hashing.open_addressing.LinearProbing;
-import hashing.open_addressing.QuadraticProbing;
+
+import sorting_algorithms.BubbleSort;
 
 class Main {
 
 	public static void main(String[] args) {
-		// Hashing -- Open Addressing -- Double Hashing
-		DoubleHashing doubleHashing = new DoubleHashing(13);
-		doubleHashing.insertKeyInHashTable("The");
-		doubleHashing.insertKeyInHashTable("quick");
-		doubleHashing.insertKeyInHashTable("brown");
-		doubleHashing.insertKeyInHashTable("fox");
-		doubleHashing.insertKeyInHashTable("over");
-		doubleHashing.insertKeyInHashTable("lazy");
-//		doubleHashing.displayHashTable();
-		doubleHashing.searchHashTable("over");
-
-		/*
-		 * // Hashing -- Open Addressing -- Quadratic Probing QuadraticProbing
-		 * quadraticProbing=new QuadraticProbing(13);
-		 * quadraticProbing.insertKeyInHashTable("The");
-		 * quadraticProbing.insertKeyInHashTable("quick");
-		 * quadraticProbing.insertKeyInHashTable("brown");
-		 * quadraticProbing.insertKeyInHashTable("fox");
-		 * quadraticProbing.insertKeyInHashTable("over");
-		 * quadraticProbing.insertKeyInHashTable("lazy"); //
-		 * quadraticProbing.displayHashTable();
-		 * quadraticProbing.searchHashTable("lazny");
-		 */
-
-		/*
-		 * // Hashing -- Open Addressing -- Linear Probing LinearProbing
-		 * linearProbing=new LinearProbing(13); linearProbing.insertInHashTable("The");
-		 * linearProbing.insertInHashTable("quick");
-		 * linearProbing.insertInHashTable("brown");
-		 * linearProbing.insertInHashTable("fox");
-		 * linearProbing.insertInHashTable("over");
-		 * 
-		 * // linearProbing.displayHashTable(); // linearProbing.searchHashTable("ox");
-		 * linearProbing.deleteKeyHashTable("over"); linearProbing.displayHashTable();
-		 */
-
-		// Hashing -- Direct Chaining
-
-		/*
-		 * DirectChaining directChaining=new DirectChaining(15);
-		 * directChaining.insertHashTable("The");
-		 * directChaining.insertHashTable("quick");
-		 * directChaining.insertHashTable("brown");
-		 * directChaining.insertHashTable("fox");
-		 * directChaining.insertHashTable("jumps");
-		 * 
-		 * directChaining.displayHashTable(); // directChaining.searchHashTable("fox");
-		 * 
-		 * directChaining.deleteKeyHashTable("fox"); directChaining.displayHashTable();
-		 */
-
+		// Sorting -- Bubble Sort
+		int[] arr= {11,31,4,66,45,32,76};
+		
+		BubbleSort bubbleSort=new BubbleSort();
+		
+		bubbleSort.bubbleSort(arr);
+		
+		bubbleSort.printArray(arr);
+		
 	}
 
 }
---
+++ src/sorting_algorithms/BubbleSort.java
@@ -0,0 +1,29 @@
+package sorting_algorithms;
+
+public class BubbleSort {
+
+	public void bubbleSort(int[] arr) {
+		int n = arr.length;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					swap(arr, j, j + 1);
+				}
+			}
+		}
+	}
+
+	private void swap(int[] arr, int i, int j) {
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+
+	public void printArray(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+}
