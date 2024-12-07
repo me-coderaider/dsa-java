d43c7400b03af22159c461e893ac0205800c5634
me-coderaider <kashyappankajnsp@gmail.com>
2024-12-07 19:12:38 +0530

    Graph - Graph Representation - using 'Adjacency List'

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 
 import java.util.ArrayList;
 
-import graph.graph_representation.using_adjacency_matrix.Graph;
-import graph.graph_representation.using_adjacency_matrix.GraphNode;
+import graph.graph_representation.using_adjacency_list.Graph;
+import graph.graph_representation.using_adjacency_list.GraphNode;
+
 
 class Main {
 
