ea6d62c61b4454e3d3b2c318626799dcbb697f1b
me-coderaider <kashyappankajnsp@gmail.com>
2024-12-11 00:14:45 +0530

    Divide and Conquer -- Number of Paths To Reach Last Cell Problem

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
-import divide_and_conquer.longest_palindromic_subsequence.LongestPalindromicSubsequence;
-import divide_and_conquer.min_cost_to_reach_last_cell.MinCostToReachLastCell;
+
+import divide_and_conquer.number_of_paths_to_reach_last_cell.NumberOfPathsToReachLastCell;
 
 class Main {
 
 	public static void main(String[] args) {
-		// Divide and Conquer -- Minimum Cost to Reach Last Cell
+		// Divide and Conquer -- Number of paths to Reach Last Cell
 
 		// we'll move in reverse manner, from last to first cell
 		int[][] array = { 
-				{ 4, 7, 8, 6, 4 }, 
-				{ 6, 7, 3, 9, 2 }, 
-				{ 3, 8, 1, 2, 4 }, 
-				{ 7, 1, 7, 3, 7 },
-				{ 2, 9, 8, 9, 3 } };
+				{ 4, 7, 1, 6 }, 
+				{ 5, 7, 3, 9 }, 
+				{ 3, 2, 1, 2 }, 
+				{ 7, 1, 6, 3} };
 		
-		MinCostToReachLastCell 	minCost=new MinCostToReachLastCell();
-		System.out.println(minCost.findMinCost(array, array.length-1, array[0].length-1)); // 36
+		int cost=25;
+		
+		NumberOfPathsToReachLastCell nop=new NumberOfPathsToReachLastCell();
+		System.out.println(nop.numberOfPaths(array, array.length-1, array[0].length-1, cost)); // 2
 		
 		
 	}
---
+++ src/divide_and_conquer/number_of_paths_to_reach_last_cell/NumberOfPathsToReachLastCell.java
@@ -0,0 +1,26 @@
+package divide_and_conquer.number_of_paths_to_reach_last_cell;
+
+public class NumberOfPathsToReachLastCell {
+	public int numberOfPaths(int[][] array, int row, int col, int cost) {
+		if (cost < 0) {
+			return 0;
+		}
+
+		if (row == 0 && col == 0) {
+			return (array[0][0] - cost == 0) ? 1 : 0;
+		}
+
+		if (row == 0) {
+			return numberOfPaths(array, 0, col - 1, cost - array[row][col]);
+		}
+
+		if (col == 0) {
+			return numberOfPaths(array, row - 1, 0, cost - array[row][col]);
+		}
+
+		int noOfPathsFromPreviousRow = numberOfPaths(array, row - 1, col, cost - array[row][col]); // moving up
+		int noOfPathsFromPreviousCol = numberOfPaths(array, row, col - 1, cost - array[row][col]); // moving left
+
+		return noOfPathsFromPreviousRow + noOfPathsFromPreviousCol;
+	}
+}
